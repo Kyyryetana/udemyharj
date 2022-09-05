85a5519431a8345d64226924995f426bf5bdce3d
nikoalander1986 <niko.alander@hotmail.com>
2022-09-05 10:20:47 +0300

    haista paska

---
+++ script.js
@@ -0,0 +1,25 @@
+//let bill = 275;
+//let bigTip = bill / 100 * 20;
+//let smallTip = bill / 100 * 15;
+//let finalValue1 = bill + smallTip;
+//let finalValue2 = bill + bigTip;
+
+//let tip = bill >= 50 && bill <= 300 ? console.log(`The bill was ${bill}, the tip was ${smallTip}, and the final value was ${finalValue1}`) : console.log(`The bill was ${bill}, the tip was ${bigTip}, and the final value was ${finalValue2}`);
+
+/////////////////////////////
+
+//const lasku = 275;
+//const tippi = lasku <= 300 && lasku >= 50 ? lasku * 0.15: lasku * 0.20;
+
+//console.log(`The bill was ${lasku}, the tip was ${tippi}, and the final value was ${lasku + tippi}`);
+
+///////////////////////////////
+
+// function calcAge(birthYear, firstName){
+//  const age = 2022 - birthYear;
+//  console.log(`${firstName} is ${age} years old`);
+//  return age;
+// }
+
+// const age = calcAge(1986, "Niko");
+// const age1 = calcAge(1985, "Anniina");
