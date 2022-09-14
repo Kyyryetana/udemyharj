1f4e4c53ab5751bd7cd8130ee23d6f316e2218a3
nikoalander1986 <niko.alander@hotmail.com>
2022-09-14 15:50:58 +0300

    paivitys

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -6,35 +6,43 @@ let highscore = 0;
 document.querySelector(".check").addEventListener("click", function () {
    const guess = Number(document.querySelector(".guess").value);
    console.log(guess, typeof guess);
-
+   //tyhjä
    if (!guess) {
       document.querySelector(".message").textContent = "Not number";
-   } else if (guess === secretNumber) {
+   } 
+   //voitto
+   else if (guess === secretNumber) {
       document.querySelector(".message").textContent = "Correct!";
       document.querySelector("body").style.backgroundColor = "#60b347";
       document.querySelector(".number").style.width = "30rem";
       document.querySelector(".number").textContent = secretNumber;
-
+      //jos highscore on isompi kuin score
       if (score > highscore) {
          highscore = score;
          document.querySelector(".highscore").textContent = highscore;
       }
-   } else if (guess < secretNumber) {
+   } 
+   //arvaus on pienempi
+   else if (guess < secretNumber) {
       if (score > 1) {
          document.querySelector(".message").textContent = "Higher";
          score--;
          document.querySelector(".score").textContent = score;
-      } else {
+      }
+      //pisteet loppui
+      else {
          document.querySelector(".message").textContent = "You lost a game...";
          document.querySelector(".score").textContent = "0";
       }
-   } else if (guess > secretNumber) {
+   }
+   //arvaus on isompi 
+   else if (guess > secretNumber) {
       document.querySelector(".message").textContent = "Lower";
       score--;
       document.querySelector(".score").textContent = score;
    }
 });
-
+//palauta tiedot, paitsi highscore
 document.querySelector('.again').addEventListener('click', function(){
    score = 20;
    secretNumber = Math.trunc(Math.random() * 20) + 1;
