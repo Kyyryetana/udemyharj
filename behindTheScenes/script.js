2b69ed7bfeb55de0700e82c6c1a4974149f1c5b7
nikoalander1986 <niko.alander@hotmail.com>
2022-09-20 15:41:47 +0300

    behindScenes

---
+++ behindTheScenes/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// function calcAge(birthYear) {
+//     const age = 2022-birthYear;
+
+//     function printAge(){
+//         const output = `${firstName} you are ${age}, born in ${birthYear}`;
+//         console.log(output);
+
+//         if(birthYear >= 1981 && birthYear <= 1996){
+//             const str = `Oh, you are a millenial ${firstName}`
+//         }
+
+//     }
+//     printAge();
+
+//     return age;
+// }
+
+// const firstName = 'Niko';
+// calcAge(1986);
+// //console.log(age);
+// //printAge();
+
+//////////////////////////////////////
+
+// const myName = 'Niko';
+
+// if (myName === 'Niko') {
+//     console.log(`Jonas is a ${job}`);
+//     const age = 2022 - 1986;
+//     console.log(age);
+//     const job = 'teacher';
+//     console.log(x);
+// }
+
+////////////////////////////////////
+
+// //variables
+// console.log(me);
+// //console.log(job);
+// //console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1986;
+
+//functions
+
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// // console.log(addArrow(2,3));
+
+// function addDecl(a,b) {
+//     return a + b;
+// }
+
+// const addExpr = function(a,b) {
+//     return a + b;
+// }
+
+// const addArrow = (a,b) => a+b;
--- project_1/script.js
+++ project_1/script.js
@@ -1,3 +1,1 @@
-function testi() {
-  //testi
-};
+'use strict'
