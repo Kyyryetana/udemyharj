055a814a6bed71169f993c6d240658f217cf108b
nikoalander1986 <niko.alander@hotmail.com>
2022-09-22 15:59:44 +0300

    duh

--- dataStructuresOperators/script.js
+++ dataStructuresOperators/script.js
@@ -26,4 +26,87 @@ const restaurant = {
          close: 24,
       },
    },
+
+   order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+   },
+
+   orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+      console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered ${address} at ${time}`);
+   },
 };
+
+restaurant.orderDelivery({
+   time: "22:30",
+   address: "Via del sole, 21",
+   mainIndex: 2,
+   starterIndex: 2,
+});
+
+const arr = [7,8,9];
+const badNewArr = [1,2,arr[0],arr[1],arr[2]];
+console.log(badNewArr);
+
+////////////////////////
+
+// //destructing Objects
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// //default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// //mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// //nested objects
+// const {
+//    fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+//////////////////////////////////////////////
+
+// //destructing arrays
+// const arr = [12, 34, 56];
+// const [a, b, c] = arr;
+// console.log(a, b, c);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// //switching variables
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// //receive 2 return values from function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// //nested destructuring
+// const nested = [1, 4, [5, 6]];
+// // const [i, ,j] = nested;
+// // console.log(i,j);
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// //default values
+// const [p = 1, q = 1, r = 1] = [8, ];
+// console.log(p, q, r);
+
+////////////////////////////////////
