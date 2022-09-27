514fc6f73f3d59a881b1f8fe9e4f6a0a5668011b
nikoalander1986 <niko.alander@hotmail.com>
2022-09-27 15:24:09 +0300

    huh

--- dataStructuresOperators/script.js
+++ dataStructuresOperators/script.js
@@ -47,33 +47,141 @@ const restaurant = {
    },
 };
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
+const airline = "TAP Air Portugal";
 
-rest
-   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-   .set("open", 11)
-   .set("close", 23)
-   .set(true, "We are open :)")
-   .set(false, "We are closed :(");
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
+//fix capitalization in name
 
-const time = 8;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+const passenger = "jOnAS"; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
 
-console.log(rest.has("categories"));
-rest.delete(2);
-// rest.clear();
-rest.set([1, 2], "Test");
-console.log(rest);
-console.log(rest.size);
+// comparing email
+const email = "hello@jonas.io";
+const loginEmail = "Hello@Jonas.Io \n";
 
-console.log(rest.get([1, 2]));
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+/////////////////////////////////////
+
+// //working with strings part 1
+// const airline = "TAP Air Portugal";
+// const plane = "A320";
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log("B737"[0]);
+
+// console.log(airline.length);
+// console.log("B737".length);
+
+// console.log(airline.indexOf("r"));
+// console.log(airline.lastIndexOf("r"));
+// console.log(airline.indexOf("Portugal"));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(" ")));
+// console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+// console.log(airline.slice(-2));
+// console.log(airline.slice(1, -1));
+
+// const checkMiddleSeat = function (seat) {
+//    //B and E are middle seats
+//    const s = seat.slice(-1);
+
+//    s === "B" || s === "E" ? console.log("You got middle seat") : console.log("You got lucky!");
+
+//    if (s === "B" || s === "E") console.log("You got middle seat");
+//    else console.log("You got lucky");
+// };
+
+// checkMiddleSeat("11B");
+// checkMiddleSeat("23C");
+// checkMiddleSeat("3E");
+
+// console.log(new String('jonas'));
+// console.log(typeof new String('jonas'));
+// console.log(typeof new String('jonas').slice(1));
+
+/////////////////////////////////////////////
+//Maps iteration
+
+// const question = new Map([
+//    ['question', 'What is the best programming language in the world?'],
+//    [1,'C'],
+//    [2,'Java'],
+//    [3,'JavaScript'],
+//    ['correct', 3],
+//    [true, 'Correct'],
+//    [false, 'Try again'],
+// ])
+// console.log(question);
+
+// //convert object to map
+// console.log(Object.entries(restaurant.openingHours));
+// const hoursMap = new Map(Object.entries(restaurant.openingHours))
+// console.log(hoursMap);
+
+// //Quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//    if(typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// // const answer = Number(prompt('your answer'));
+// // console.log(answer);
+
+// // console.log(question.get(question.get('correct') === answer));
+
+// //convert map to array
+// console.log(...question);
+// // console.log(...question.entries());
+// console.log(...question.keys());
+// console.log(...question.values());
+
+////////////////////////////////
+//Maps: fundamentals
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// rest.set(2, "Lisbon, Portugal");
+
+// rest
+//    .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//    .set("open", 11)
+//    .set("close", 23)
+//    .set(true, "We are open :)")
+//    .set(false, "We are closed :(");
+
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 8;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has("categories"));
+// rest.delete(2);
+// // rest.clear();
+
+// const arr = [1,2];
+// rest.set(arr, "Test");
+// rest.set(document.querySelector('h1'), 'Heading')
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
 
 ////////////////////////////////////////
 
