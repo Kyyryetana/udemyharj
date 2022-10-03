b1ca9500dbf645785492de97696af8fa57779bba
nikoalander1986 <niko.alander@hotmail.com>
2022-10-03 16:00:44 +0300

    workingWithArrays

--- 10functions/script.js
+++ 10functions/script.js
@@ -1,20 +1,70 @@
 "use strict";
 
-const secureBooking = function() {
-    let passengerCount = 0;
 
-    return function() {
-        passengerCount++
-        console.log(`${passengerCount} passengers`);
-    }
-}
 
-const booker = secureBooking();
-booker();
-booker();
-booker();
+///////////////////////////////////
 
-console.dir(booker);
+// let f;
+
+// const g = function() {
+//     const a = 23;
+//     f = function() {
+//         console.log(a*2);
+//     }
+// }
+
+// const h = function() {
+//     const b = 777;
+//     f= function() {
+//         console.log(b*2);
+//     }
+// }
+
+// g();
+// f();
+// console.dir(f);
+
+// //re-assign f function
+// h();
+// f();
+// console.dir(f);
+
+// //Example 2
+
+// const boardPassengers = function(n,wait) {
+//     const perGroup = n/3;
+    
+//     setTimeout(function(){
+//         console.log(`We are now boarding all ${n} passengers`);
+//         console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//     }, wait * 1000)
+    
+//     console.log(`will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(180,3)
+
+
+
+
+//////////////////////////////////
+
+// const secureBooking = function () {
+//    let passengerCount = 0;
+
+//    return function () {
+//       passengerCount++;
+//       console.log(`${passengerCount} passengers`);
+//    };
+// };
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
 
 ////////////////////////////////
 
